51609ea84b2211a78a01a7afcb795c7d557bb7a8
Marvin Kome <marvinkome@gmail.com>
2023-12-09 22:08:00 +0100

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Marvin Kome on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
